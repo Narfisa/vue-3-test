ee7c3b1b19d5ed4b056ee143c5c2ce1bd191616c
Narfisa <valya.okulova@mail.ru>
2023-12-21 14:02:31 +0700

    setWindowType

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts">
+<script lang="ts">
 import AppHeader from './components/AppHeader.vue'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    components: {
+        AppHeader
+    },
+    mounted () {
+        this.calcWindowType()
+        window.addEventListener('resize', this.onResizeWindow.bind(this))
+    },
+    methods: {
+        onResizeWindow () {
+            this.calcWindowType()
+        },
+        calcWindowType () {
+            const windowWidth = window.innerWidth
+            if (windowWidth < 640) {
+                this.setWindowType('mobile')
+            } else if (windowWidth < 1025) {
+                this.setWindowType('tablet')
+            } else if (windowWidth < 1367) {
+                this.setWindowType('laptop')
+            } else if (windowWidth <= 1920) {
+                this.setWindowType('desktop')
+            } else {
+                this.setWindowType('largeDesktop')
+            }
+        },
+        setWindowType (type:string) {
+            if (type !== this.$store.state.app.windowType) {
+                this.$store.commit('app/setWindowType', type)
+            }
+        }
+    }
+})
 </script>
 
 <template>
--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,18 @@
+import stateT from '@interface/store/app' 
+
+const state = {
+    currentPage: null,
+    windowType: 'desktop'
+}
+
+const mutations = {
+    setCurrentPage (state:stateT, value:string) { state.currentPage = value },
+    setWindowType (state:stateT, value:string) { state.windowType = value }
+}
+
 export default {
     namespaced: true,
-    state: {},
+    state,
     actions: {},
-    mutations: {},
+    mutations
 }
