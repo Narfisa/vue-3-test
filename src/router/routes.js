fca0aa50daf9f5e5a59fa6d4387537e442e70722
Narfisa <valya.okulova@mail.ru>
2023-12-28 13:35:10 +0700

    added alias to routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,17 +3,17 @@ const routes = [
     {
         path: '/home',
         name: 'Home',
-        component: () => import('../views/Main.vue'),
+        component: () => import('@views/Main.vue'),
     },
     {
         path: '/directory',
         name: 'Directory',
-        component: () => import('../views/Directory.vue'),
+        component: () => import('@views/Directory.vue'),
     },
     {
         path: '/favorites',
         name: 'Favorites',
-        component: () => import('../views/Favorites.vue'),
+        component: () => import('@views/Favorites.vue'),
     }
 ]
 
